49404042591fc2d2048a6257345a64979cc6aaf6
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-04-27 12:52:02 -0400

    Adding tests for the 2 new programs

--- tests/test_output_qpu_embed.py
+++ tests/test_output_qpu_embed.py
@@ -21,7 +21,7 @@ example_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class TestSmoke(unittest.TestCase):
     # test that the example runs without failing
     def test_smoke(self):
-        file_path = os.path.join(example_dir, 'lazy_fixed_embedding_composite.py')
+        file_path = os.path.join(example_dir, 'qpu_embed.py')
 
         value = subprocess.check_output(["python", file_path])
 
