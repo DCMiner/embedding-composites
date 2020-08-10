e12b9f71af172bffc060301c824ce8fcfdbb153c
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-08-10 08:42:59 -0400

    Making Chimera clear

--- lazy_fixed_embedding_composite_chimera.py
+++ lazy_fixed_embedding_composite_chimera.py
@@ -14,13 +14,14 @@
 
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import LazyFixedEmbeddingComposite
+import sys
 
 # Set up the QUBO. Start with the equations from the slides:
-chainstrength = 4
+chainstrength = float(sys.argv[1])
 numruns = 1000
 Q = {(0, 0): 2, (0, 1): -2, (0, 2): -2, (1, 2): 2}
 
-sampler = LazyFixedEmbeddingComposite(DWaveSampler(solver={'qpu': True}))
+sampler = LazyFixedEmbeddingComposite(DWaveSampler(solver={'topology__type__eq': 'chimera'}))
 response = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=numruns)
 print(sampler.properties['embedding'])
 
