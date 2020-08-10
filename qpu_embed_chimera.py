36e35a2e310ef009ecfc0981ac861bcf20996b2b
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-08-10 11:14:06 -0400

    Cleaning up offset

--- qpu_embed_chimera.py
+++ qpu_embed_chimera.py
@@ -1,6 +1,7 @@
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
 import sys
+import dimod
 
 # Set up the QUBO. Start with the equations for the three qubits:
 # x + y - 2xy -1
@@ -11,9 +12,10 @@ import sys
 chainstrength = float(sys.argv[1])
 numruns = 1000
 Q = {(0, 0): 2, (0, 1): -2, (0, 2): -2, (1, 2): 2}
+bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=-2)
 
 sampler = EmbeddingComposite(DWaveSampler(solver={'topology__type__eq': 'chimera'}))
-response = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=numruns)
+response = sampler.sample(bqm, chain_strength=chainstrength, num_reads=numruns)
 
 for smpl, energy in response.data(['sample', 'energy']):
     print(smpl, energy)
