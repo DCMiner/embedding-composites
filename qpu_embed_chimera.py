78c375b0f2b377680ec4b01c112c05db7a97745b
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-08-10 08:39:01 -0400

    Pointing to Chimera and add cmd-line arg

--- qpu_embed_chimera.py
+++ qpu_embed_chimera.py
@@ -1,5 +1,6 @@
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
+import sys
 
 # Set up the QUBO. Start with the equations for the three qubits:
 # x + y - 2xy -1
@@ -7,11 +8,11 @@ from dwave.system.composites import EmbeddingComposite
 # -2zx + z + x - 1
 # QUBO: 2x - 2xy + 2yz - 2zx - 2
 
-chainstrength = 5
+chainstrength = float(sys.argv[1])
 numruns = 1000
 Q = {(0, 0): 2, (0, 1): -2, (0, 2): -2, (1, 2): 2}
 
-sampler = EmbeddingComposite(DWaveSampler(solver={'qpu': True}))
+sampler = EmbeddingComposite(DWaveSampler(solver={'topology__type__eq': 'chimera'}))
 response = sampler.sample_qubo(Q, chain_strength=chainstrength, num_reads=numruns)
 
 for smpl, energy in response.data(['sample', 'energy']):
